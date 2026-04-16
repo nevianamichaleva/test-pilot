4a590a9c3f1227f72824db608a57009652f416a5
nevyana <nevyana@composity.com>
2026-04-16 18:22:53 +0300

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "Test Pilot",
   description: "Test Pilot",
+  icons: {
+    icon: [{ url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" }],
+    shortcut: ["/images/favicon-32x32.png"],
+    apple: ["/images/favicon-32x32.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
